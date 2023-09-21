a6a0d0f94d5b03ae090233f43c1498a964023924
mbhesam <m.b.hesam>
2023-09-21 13:57:23 +0330

    add shared_tasks decorator

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,9 +1,12 @@
 from tasks.tasks_utils import create_update
+from celery import shared_task
 
+@shared_task
 def scheduled_task_run():
     check_task_execution()
     create_update()
 
+@shared_task
 def check_task_execution():
     with open("test_hesam.txt","w") as writer:
         writer.write("it executed write")
